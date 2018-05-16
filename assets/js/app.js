fb6f623af9cc48d7fc9ec786c17afa4bf4450e38
soeren <27072037+leeander@users.noreply.github.com>
2018-05-17 00:52:21 +0200

    hui

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+import {MDCRipple} from '@material/ripple';
+import {MDCToolbar} from '@material/toolbar';
+import {MDCTemporaryDrawer} from '@material/drawer';
+
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
+const toolbar = new MDCToolbar(document.querySelector('.mdc-toolbar'));
+
+const drawer = new MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
+document.querySelector('.menu').addEventListener('click', () => drawer.open = true);
